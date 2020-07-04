7af471fa9d32bb0bec712a1b3366cb7b4fb2f94f
Rafael Belo <rafaelrpbelo@gmail.com>
2020-07-04 14:14:09 -0300

    Enable player to walk on a plane

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    NavMeshAgent navMeshAgent;
+    public Camera cam;
+    
     void Start()
     {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
     }
 }
